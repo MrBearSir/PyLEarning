b7eee1d818d42d1ae6a8ddc726bb7981152d35eb
Oleg <dobrunja90@yahoo.ca>
2023-10-24 21:23:04 -0400

    added feature to letter-grade depending on inputed floating score in 0.0-1.0 range

--- loops.py
+++ loops.py
@@ -1,20 +1,19 @@
+hrs = input("Enter Hours:")
 try:
-    hrs = input("Enter Hours:")
-    try:
-        h = float(hrs)
-    except:
-        print("Please input your hours using numbers")
-    rate = input("Enter Your Rate of Pay:")
-    try:
-        rate = float(rate)
-    except:
-        print("Please enter your rate using numbers ")
-    if h <= 40.0:
-        total = h*rate
-        print(total)
-    else:
-        h = h - 40
-        total = 40*rate+rate*1.5*h
-        print(total)
+    h = float(hrs)
 except:
-    print("PLease try again")
+    print("Please input your hours using numbers")
+    quit()
+rate = input("Enter Your Rate of Pay:")
+try:
+    rate = float(rate)
+except:
+    print("Please enter your rate using numbers ")
+    quit()
+if h <= 40.0:
+    total = h*rate
+    print(total)
+else:
+    h = h - 40
+    total = 40*rate+rate*1.5*h
+    print(total)
