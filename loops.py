70e9f4f0fc6778c7b10db1f84a96e626d2e3b4dd
Oleg <dobrunja90@yahoo.ca>
2023-10-24 20:54:47 -0400

    Added loops.py as a wage calc with overtime capability after 40 hrs and catch case for wrong input

---
+++ loops.py
@@ -0,0 +1,17 @@
+hrs = input("Enter Hours:")
+try:
+    h = float(hrs)
+except:
+    print("Please input your hours using numbers")
+rate = input("Enter Your Rate of Pay:")
+try:
+    rate = float(rate)
+except:
+    print("Please enter your rate using numbers ")
+if h <= 40.0:
+    total = h*rate
+    print(total)
+else:
+    h = h - 40
+    total = 40*rate+rate*1.5*h
+    print(total)
--- payCalc.py
+++ payCalc.py
@@ -2,4 +2,4 @@
 hrs = input("Enter Hours:")
 rate = input("Enter rate:")
 pay = float(hrs)*float(rate)
-print("Pay: "+ str(pay))
+print("Pay: "+ str(pay)) 
