6b2e02f941960a22d3701cf366e011eb75b3c946
Oleg <dobrunja90@yahoo.ca>
2023-10-24 21:00:21 -0400

    added catch if program cant run correctly

--- loops.py
+++ loops.py
@@ -1,17 +1,20 @@
-hrs = input("Enter Hours:")
 try:
-    h = float(hrs)
+    hrs = input("Enter Hours:")
+    try:
+        h = float(hrs)
+    except:
+        print("Please input your hours using numbers")
+    rate = input("Enter Your Rate of Pay:")
+    try:
+        rate = float(rate)
+    except:
+        print("Please enter your rate using numbers ")
+    if h <= 40.0:
+        total = h*rate
+        print(total)
+    else:
+        h = h - 40
+        total = 40*rate+rate*1.5*h
+        print(total)
 except:
-    print("Please input your hours using numbers")
-rate = input("Enter Your Rate of Pay:")
-try:
-    rate = float(rate)
-except:
-    print("Please enter your rate using numbers ")
-if h <= 40.0:
-    total = h*rate
-    print(total)
-else:
-    h = h - 40
-    total = 40*rate+rate*1.5*h
-    print(total)
+    print("PLease try again")
